c14e2f8d2baa2a23bd916944966b5671f56116d7
rdrgzma <rdrgzma@hotmail.com>
2017-12-06 00:20:01 -0200

    add profile dev and mysql DB

---
+++ src/main/java/com/comanda/server/config/DevConfig.java
@@ -0,0 +1,26 @@
+package com.comanda.server.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.comanda.server.services.DBTestService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBTestService dbTestService;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		
+		dbTestService.instantiateTestDatabase();
+		return true;
+	}
+
+}
